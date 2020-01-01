a49f388020660b364ff0487268ca102d3397b33e
NekoChen <38178384+NekoChen@users.noreply.github.com>
2020-01-01 13:40:50 +0800

    增加了我的关注部分 （界面没做，惨不忍睹先意会一下） 实现了取消关注，添加关注都还没写

---
+++ src/main/java/server/FollowServer.java
@@ -0,0 +1,59 @@
+package server;
+
+
+import dao.AlbumDAO;
+import dao.CommentDAO;
+import dao.FollowDAO;
+import entity.DataResult;
+import model.Follow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class FollowServer {
+    public static DataResult getFollowInfoById(String fromId){
+        DataResult dataResult = new DataResult();
+
+        dataResult.setStatus(0);
+        dataResult.setMsg("get follow info");
+        List<Follow> followList = new ArrayList<>();
+        List<Map<String,Object>> maps = FollowDAO.getFollowInfoListById(fromId);
+        for(Map<String,Object>map:maps){
+            followList.add(new Follow(map));
+        }
+
+        dataResult.setMsg("get comment success");
+        dataResult.setData(followList);
+
+        return dataResult;
+    }
+
+    public static DataResult addFollow(String fromId,String toId){
+        DataResult dataResult = new DataResult();
+        boolean res = FollowDAO.addFollow(fromId,toId);
+        if(res!=true){
+            dataResult.setStatus(0);
+            dataResult.setMsg("add new follow success");
+//            Follow follow = new Follow(FollowDAO.getFollowInfoListById(fromId,toId));
+//            dataResult.setData(follow);
+        }else{
+            dataResult.setStatus(-1);
+            dataResult.setMsg("add new follow error");
+        }
+        return dataResult;
+    }
+
+    public static DataResult deleteFollow(String fromId,String toId){
+        DataResult dataResult = new DataResult();
+        boolean res = FollowDAO.delFollow(fromId,toId);
+        if(res!=false){
+            dataResult.setStatus(0);
+            dataResult.setMsg("delete follow success");
+        }else{
+            dataResult.setStatus(-1);
+            dataResult.setMsg("delete follow error");
+        }
+        return dataResult;
+    }
+}
